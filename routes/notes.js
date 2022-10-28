ecd1efbd3531d49625a80f12e3fe93b28e9c9566
josephjensen <joe@cryptonumerics.com>
2022-10-28 16:31:26 -0700

    added post with id

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,29 @@
 const notes = require('express').Router();
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
 
 // GET Route for retrieving all the notes
 notes.get('/', (req, res) =>
-  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 );
+
+notes.post('/', (req, res) => {
+    const { title, text } = req.body;
+    const newNote  = {
+        title,
+        text,
+        id: uuid(),
+    };
+    readAndAppend(newNote, './db/db.json');
+
+
+    const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      res.json(response);
+    }
+)
 
 module.exports = notes;
