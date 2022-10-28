9bbc7014326d62190d1c3edc369706c93950ff00
josephjensen <joe@cryptonumerics.com>
2022-10-28 16:09:58 -0700

    add helper functions, add routes for notes. just get for now

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const path = require('path')
+const api = require('./routes/index')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 
 app.use(express.static('public'));
 
